963f609571261e81f4b614d53874ed95f24de83c
songchanghui <songchanghui@nowledgedata.com.cn>
2019-02-18 21:16:58 +0800

    修改readme

--- trains-graph/src/main/java/com/thoughtworks/graphx/util/ParmUtil.java
+++ trains-graph/src/main/java/com/thoughtworks/graphx/util/ParmUtil.java
@@ -8,7 +8,7 @@ public class ParmUtil {
     public static final int ZERO = 0;
     public static final int ONE = 1;
     public static final int TWO = 2;
-    public static final int THREE = 2;
+    public static final int THREE = 3;
     public static Object[] addElement(Object[] objects, Object element, int index) {
         int length = objects.length;
         Object destination[] = new Object[length + 1];
